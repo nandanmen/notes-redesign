78dd4e856b5ffd5c290c62dbedb8735069d86965
Narendras <n.syahrasyad.n@gmail.com>
2018-02-06 22:19:57 -0800

    Added trees!

---
+++ notes/221/tree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(root) {
+    this.root = root;
+  }
+}
