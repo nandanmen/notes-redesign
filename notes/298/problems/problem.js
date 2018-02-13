47180670cb4ae288f61810a05e846d638b2da597
Narendras <n.syahrasyad.n@gmail.com>
2018-02-13 00:38:33 -0800

    Added show solution animations

--- notes/298/problems/problem.js
+++ notes/298/problems/problem.js
@@ -1,27 +1,55 @@
 (function main() {
   const btn = document.querySelector(".problem__show");
+  setBtnHover(btn);
   btn.addEventListener("click", () => {
-    const problems = document.querySelector(".problems");
+    const header = document.querySelector(".problem__header");
+    const solution = document.querySelector(".solution__container");
+    const data = document.querySelector(".problem__data");
     if (btn.textContent == "Show Solution") {
-      problems.classList.add("solutions");
-      toggleSolution();
+      solution.classList.add("solution--active");
+      data.style.transform = "translateX(2vw)";
+      setTimeout(() => {
+        header.classList.add("problem__header--act");
+        btn.classList.add("problem__hide");
+      }, 400);
       btn.textContent = "Hide Solution";
     } else {
-      problems.classList.remove("solutions");
-      toggleSolution();
+      header.classList.remove("problem__header--act");
+      data.style.transform = "translateX(0)";
+      solution.classList.remove("solution--active");
+      btn.classList.remove("problem__hide");
       btn.textContent = "Show Solution";
     }
-  })
+    toggleSolution();
+  });
 }());
+
+function setBtnHover(el) {
+  const solution = document.querySelector(".solution__container");
+  el.addEventListener("mouseover", () => {
+    const isActive = solution.classList.contains("solution--active");
+    if (!isActive) {
+      solution.style.transform = "translateY(97vh)";
+    }
+  });
+  el.addEventListener("mouseout", () => {
+    const isActive = solution.classList.contains("solution--active");
+    if (!isActive) {
+      solution.style.transform = "translateY(100vh)";
+    }
+  });
+}
 
 function toggleSolution() {
   const question = document.querySelector(".problem__info");
-  const solution = document.querySelector(".problem__solution");
+  const solution = document.querySelector(".solution__container");
   if (question.style.opacity === "0") {
-    solution.style.opacity = 0;
+    solution.style.transform = "translateY(100vh)";
     question.style.opacity = 1;
   } else {
-    question.style.opacity = 0;
-    solution.style.opacity = 1;
+    setTimeout(() => {
+      question.style.opacity = 0;
+    }, 400);
+    solution.style.transform = "translateY(0)";
   }
 }
