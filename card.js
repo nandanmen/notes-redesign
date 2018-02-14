92ebc80b97e45ca8be55fd9b67099bd1349f7f25
Narendras <n.syahrasyad.n@gmail.com>
2018-02-13 17:39:11 -0800

    Revised home page

--- card.js
+++ card.js
@@ -1,154 +1,39 @@
+'use strict'
+
 class Card {
-  constructor(dept, course, desc) {
+  constructor(dept, name, desc) {
     this.dept = dept;
-    this.course = course;
+    this.name = name;
     this.desc = desc;
   }
 
-  renderDept() {
-    const container = document.createElement("h2");
-    const text = document.createTextNode(this.dept);
-    container.classList.add("card__dept");
-    container.appendChild(text);
-    return container;
-  }
+  render(target) {
+    const template = document.getElementById("card-template");
+    const title = template.content.querySelector(".card__title");
+    const dept = template.content.querySelector(".card__dept");
+    const desc = template.content.querySelector(".card__desc");
+    const link = template.content.querySelector(".card__link");
 
-  renderCourse() {
-    const container = document.createElement("h1");
-    const text = document.createTextNode(this.course);
-    container.classList.add("card__course");
-    container.appendChild(text);
-    return container;
-  }
+    title.textContent = this.name;
+    dept.textContent = this.dept;
+    desc.textContent = this.desc;
+    link.href = `notes/${this.name}`;
 
-  renderDesc() {
-    const container = document.createElement("p");
-    const text = document.createTextNode(this.desc);
-    container.classList.add("card__desc");
-    container.appendChild(text);
-    return container;
-  }
-
-  renderLink() {
-    const a = document.createElement("a");
-    const span = document.createElement("span");
-    a.href = "notes/" + this.course;
-    a.appendChild(span);
-    return a;
-  }
-
-  /*
-  * Renders the contents of this card onto the DOM
-  */
-  renderCard() {
-    const card = document.querySelector(".card");
-    const contentContainer = document.querySelector(".card__content");
-
-    const dept = this.renderDept();
-    const course = this.renderCourse();
-    const desc = this.renderDesc();
-    const a = this.renderLink();
-
-    contentContainer.appendChild(dept);
-    contentContainer.appendChild(course);
-    contentContainer.appendChild(desc);
-
-    card.appendChild(a);
-  }
-
-  /*
-  * Removes this card from the DOM
-  */
-  destroy() {
-    const card = document.querySelector(".card");
-    const content = document.querySelector(".card__content");
-
-    const dept = document.querySelector(".card__dept");
-    const course = document.querySelector(".card__course");
-    const desc = document.querySelector(".card__desc");
-    const a = document.getElementsByTagName("a")[0];
-
-    content.removeChild(dept);
-    content.removeChild(course);
-    content.removeChild(desc);
-    card.removeChild(a);
+    const result = document.importNode(template.content, true);
+    target.appendChild(result);
   }
 }
 
-class CardList {
-  constructor() {
-    this.cards = [];
-    this.active = 0;
-    this.length = 0;
-  }
-
-  add(card) {
-    this.cards.push(card);
-    this.length++;
-  }
-
-  remove(card) {
-    const indexToRemove = this.cards.indexOf(card);
-    const newArray = this.cards.splice(indexToRemove, 1);
-    this.cards = newArray;
-  }
-
-  destroyActive() {
-    const active = this.cards[this.active];
-    active.destroy();
-  }
-
-  activateNext() {
-    if (this.active + 1 < this.length) {
-      this.destroyActive();
-
-      const result = this.cards[this.active + 1];
-      this.active++;
-      result.renderCard();
-
-      return result;
-    }
-  }
-
-  activatePrev() {
-    if (this.active - 1 >= 0) {
-      this.destroyActive();
-
-      const result = this.cards[this.active - 1];
-      this.active--;
-      result.renderCard();
-
-      return result;
-    }
-  }
-
-  render() {
-    const card = this.cards[this.active];
-    card.renderCard();
-  }
-
-}
-
-function main() {
-  const cardList = new CardList();
+(function main() {
   const card210 = new Card("CPSC", 210, "Software Construction");
-  const card221 = new Card("CPSC", 221, "Beginner Algorithms and Data Structures");
   const card213 = new Card("CPSC", 213, "Introduction to Computer Systems");
+  const card221 = new Card("CPSC", 221, "Beginner Algorithms and Data Structures");
+  const container = document.querySelector(".cards");
+  card210.render(container);
+  card213.render(container);
+  card221.render(container);
 
-  cardList.add(card210);
-  cardList.add(card221);
-  cardList.add(card213);
-
-  cardList.render();
-
-  const btnNext = document.querySelector(".btn--next");
-  const btnPrev = document.querySelector(".btn--prev");
-  btnNext.addEventListener("click", () => {
-    cardList.activateNext();
-  });
-  btnPrev.addEventListener("click", () => {
-    cardList.activatePrev();
-  });
-}
-
-document.body.onload = main;
+  card221.render(container);
+  card221.render(container);
+  card221.render(container);
+}());
